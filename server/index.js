b40339d409f7cd114a4c7aad4ce6aca4807c33c8
yadukrishnan-t <yadukrishnant@outlook.com>
2023-11-17 13:58:47 -0500

    port-3000

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
